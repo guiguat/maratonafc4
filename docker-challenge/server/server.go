c3137bf7e4fd60ccefdd07b93d0d474522f979db
guiguat <guaturadzn@gmail.com>
2020-08-27 23:27:29 -0300

    create simple go webserver

--- docker-challenge/server/server.go
+++ docker-challenge/server/server.go
@@ -1,9 +1,18 @@
 package main
+
 import (
-	"fmt",
+	"fmt"
 	"net/http"
 )
 
-func main(){
+func hello(w http.ResponseWriter, r* http.Request){
+	fmt.Fprintf(
+		w,
+		"Eu sou Full Cycle.",
+	)
+}
 
+func main(){
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
 }
