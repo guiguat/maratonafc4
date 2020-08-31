6635f3164aad9c7ee43794e6753b08222fe898a6
guiguat <guaturadzn@gmail.com>
2020-08-30 22:42:22 -0300

    create redirect route

--- reactjs-challenge/src/routes.tsx
+++ reactjs-challenge/src/routes.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Users from "./Pages/Users";
 
 const Routes: React.FC = () => {
   return (
     <Router>
       <Switch>
+        <Route path="/" exact>
+          <Redirect to="/users" />
+        </Route>
         <Route path="/users">
           <Users />
         </Route>
