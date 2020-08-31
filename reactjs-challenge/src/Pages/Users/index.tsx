b913ff1b7963ba9992210116f097e8db4ff9e8bf
guiguat <guaturadzn@gmail.com>
2020-08-30 22:42:31 -0300

    create users page

--- reactjs-challenge/src/Pages/Users/index.tsx
+++ reactjs-challenge/src/Pages/Users/index.tsx
@@ -1,9 +1,42 @@
-import React from "react";
-
-// import { Container } from './styles';
-
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "./styles.css";
+interface user {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
 const Users: React.FC = () => {
-  return <h1>Users</h1>;
+  const [users, setUsers] = useState<Array<user> | null>(null);
+
+  useEffect(() => {
+    Axios.get("https://reqres.in/api/users").then((res) =>
+      setUsers(res.data.data)
+    );
+  }, []);
+
+  if (!users) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div className="app">
+      <h1>Listagem de usuarios</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id} className="user-container">
+            <img src={user.avatar} alt={user.first_name} className="avatar" />
+            <div className="user-content">
+              <h3>{`${user.first_name} ${user.last_name}`}</h3>
+              <p>{user.email}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Users;
